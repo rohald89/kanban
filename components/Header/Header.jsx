3d739efcecb4619d4e776012ca61751a421f7c0e
Rohald van Merode <rohald_89@live.nl>
2022-06-11 15:07:54 +0200

    created the header element

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import useWindowSize from "../../hooks/useWindowSize";
 import Button from "../shared/Button";
 
@@ -5,10 +6,35 @@ const Header = () => {
   const {width} = useWindowSize();
 
   return (
-    <header className="flex bg-white items-center p-4">
-        <img src="../../assets/logo-light.svg" alt="" />
-        <h2 className="heading-lg">Platform Launch</h2>
-
+    <header className="flex bg-white justify-between items-center p-4 border-b border-lightGreyLine dark:bg-darkGrey dark:text-white dark:border-darkGreyLine">
+        <div className="flex items-center gap-2">
+        {
+            width <= 768 && (
+                <Image src="/logo-mobile.svg" alt="kanban logo" height={25} width={24}  />
+            )
+        }
+        <h2 className="heading-lg">Platform Launch
+        </h2>
+        {
+            width <= 768 && (
+                <Image src="/icon-chevron-down.svg" alt="chevron" height={4} width={8}  />
+            )
+        }
+        </div>
+        <div className="flex items-center gap-4">
+        {
+            width > 768 ? (
+                <Button className="btn btn__primary btn-lg">
+                    + Add New Task
+                </Button>
+            ) : (
+                <Button className="btn btn__primary px-5 flex justify-center items-center">
+                    <Image src="/icon-add-task-mobile.svg" height={12} width={12} />
+                </Button>
+            )
+        }
+        <Image src="/icon-vertical-ellipsis.svg" height={16} width={4} />
+        </div>
     </header>
   )
 }
