52da99578d011bc6a30b8ef39289449800287b64
Rohald van Merode <rohald_89@live.nl>
2022-06-11 20:02:01 +0200

    created Modal component, made it accessible and added animations

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
+import Modal from "@components/Modal";
 import Image from "next/image";
-import useWindowSize from "../../hooks/useWindowSize";
+import { useRef, useState } from "react";
+import useWindowSize from "@hooks/useWindowSize";
 import Button from "../shared/Button";
+import useOnClickOutside from "@hooks/useOnClickOutside";
 
 const Header = () => {
   const {width} = useWindowSize();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const mobileMenu = useRef();
+  useOnClickOutside(mobileMenu, () => setShowMenu(false));
 
   return (
     <header className="flex bg-white justify-between items-center p-4 border-b border-lightGreyLine dark:bg-darkGrey dark:text-white dark:border-darkGreyLine">
@@ -13,14 +20,18 @@ const Header = () => {
                 <Image src="/logo-mobile.svg" alt="kanban logo" height={25} width={24}  />
             )
         }
-            <div className="flex justify-center items-center">
+            <button className="flex justify-center items-center" onClick={() => setShowMenu(true)}>
                 <h2 className="heading-lg ml-5 mr-2">Platform Launch</h2>
                 {
                     width <= 768 && (
                         <Image src="/icon-chevron-down.svg" alt="chevron" height={4} width={8} />
                     )
                 }
-            </div>
+            </button>
+                <Modal show={showMenu} onClose={() => setShowMenu(!showMenu)}>
+                    <div ref={mobileMenu} className="h-96 w-96 bg-white">TEST</div>
+                </Modal>
+
         </div>
         <div className="flex items-center gap-4">
         {
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { CSSTransition } from "react-transition-group";
+
+const Modal = (props) => {
+  const closeOnEscapeKeyDown = (e) => {
+    if ((e.charCode || e.keyCode) === 27) {
+      props.onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener("keydown", closeOnEscapeKeyDown);
+    return function cleanup() {
+      document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
+    };
+  }, []);
+
+  return (
+    <CSSTransition
+      in={props.show}
+      unmountOnExit
+      timeout={{ enter: 0, exit: 300 }}
+    >
+      <div
+        className={`modal opacity-0 bg-opacity-50 fixed flex transition duration-800 left-0 right-0 top-0 bottom-0 bg-black z-10 items-center justify-center`}
+      >
+        <div className="modal-content -translate-y-[200px] transition">
+          {props.children}
+        </div>
+      </div>
+    </CSSTransition>
+  );
+};
+export default Modal;
