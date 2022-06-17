0bd4c5528463d3abe98175911a57b4467119aaba
Rohald van Merode <rohald_89@live.nl>
2022-06-18 00:17:40 +0200

    fixed the sidebarToggle to properly show up on both states

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,8 +13,8 @@ const Header = ({ sidebarVisible }) => {
   const {theme} = useTheme();
 
   return (
-    <header className="flex bg-white justify-between items-center p-4 border-b border-lightGreyLine dark:bg-darkGrey dark:text-white dark:border-darkGreyLine">
-        <div className="flex items-center">
+    <header className="p-4 flex bg-white justify-between items-center border-b border-lightGreyLine dark:bg-darkGrey dark:text-white dark:border-darkGreyLine">
+        <div className="flex items-center ">
         {
             width <= 768 ? (
                 <>
@@ -35,7 +35,7 @@ const Header = ({ sidebarVisible }) => {
                 </>
             ) : (
                 <>
-                    <div className={`ml-6 ${sidebarVisible ? 'w-[300px]' : 'w-[200px]'} border-r-2 border-r-darkGreyLine`}>
+                    <div className={`${sidebarVisible ? 'w-[300px]' : 'w-[200px]'} py-2.5 box-border transition border-r border-r-darkGreyLine`}>
                         <Image src={theme === 'dark' ? "/logo-light.svg" : "/logo-dark.svg"} alt="kanban logo" height={25} width={152}  />
                     </div>
                     <h2 className="heading-lg ml-5 mr-2">{currentBoard.name}</h2>
@@ -46,8 +46,8 @@ const Header = ({ sidebarVisible }) => {
 
         </div>
         <div className="flex items-center gap-4">
-        <AddNewTask />
-        <Image src="/icon-vertical-ellipsis.svg" height={16} width={4} />
+            <AddNewTask />
+            <Image src="/icon-vertical-ellipsis.svg" height={16} width={4} />
         </div>
     </header>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     // <div className="h-screen grid grid-rows-[64px_1fr] md:grid-rows-[80px_1fr] md:grid-cols-[minmax(0,_300px)_1fr]">
     <div className="h-screen">
       <Header sidebarVisible={showSidebar} />
-      <div className="flex ">
+      <div className="flex board-height">
         <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
         <Board sidebarVisible={showSidebar} />
       </div>
